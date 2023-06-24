7b62ba2e30b83bb18ce6c01155e76b677b65a54d
Karen Miano <mianokaren1798@gmail.com>
2023-06-24 03:32:23 -0700

    Printing

---
+++ print_func.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * _printf - prints to stdout
+ * @format: the string to be printed
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	char d;
+	int count = 0;
+	char *str;
+
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			switch (*(format + 1))
+			{
+				case 'c':
+				{
+					d = va_arg(args, int);
+					count += write(1, &d, 1);
+					format += 2;
+					break;
+				}
+				case 's':
+				{
+					str = va_arg(args, char *);
+					count += write(1, str, _strlen(str));
+					format += 2;
+					break;
+				}
+			}
+		}
+			count += write(1, &(*format), 1);
+			format++;
+	}
+	return (count);
+}
+
